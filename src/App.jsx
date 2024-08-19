272e902df936b217cacca26571598a10c9d865b2
Julie Lacresse <Julie.Lacresse@nvision.lu>
2024-08-19 11:40:06 +0200

    page welcome

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Contact from "./pages/Contact";
 import Layout from "./pages/Layout"
 import SignUp from "./pages/SignUp";
 import Onboarding from "./pages/Onboarding";
+import Welcome from "./pages/Welcome";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/welcome" element={<Welcome/>} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="contact" element={<Contact />} />
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
- 
-export default function Button({txt, variant="base", className, onClick}) {
- 
-  let classBtn = ""
- 
-  if (variant == "base"){
-    classBtn = "bg-primary text-white hover:bg-secondary hover:text-primary"
-  } else if (variant == "tertiary") {
-    classBtn = "bg-white text-primary border border-solid border-primary"
-  }
- 
- 
-  return (
-    <button onClick = {onClick} className= {classBtn + ' px-12 py-4 text-button rounded-full ' + className}>{txt}</button>
-  )
+import React from "react";
+
+export default function Button({ txt, variant = "base", className, onClick }) {
+	let classBtn = "";
+
+	if (variant == "base") {
+		classBtn = "bg-primary text-white hover:bg-secondary hover:text-primary";
+	} else if (variant == "tertiary") {
+		classBtn = "bg-white text-primary border border-solid border-primary";
+	} else if (variant == "welcome_primary") {
+		classBtn =
+			"bg-primary text-white border border-solid border-white hover:bg-white hover:text-primary";
+	} else if (variant == "welcome_secondary") {
+		classBtn = "bg-white text-primary hover:bg-secondary";
+	}
+
+	return (
+		<button
+			onClick={onClick}
+			className={
+				classBtn +
+				"  px-12 py-4 text-button rounded-full font-poppins " +
+				className
+			}
+		>
+			{txt}
+		</button>
+	);
 }
