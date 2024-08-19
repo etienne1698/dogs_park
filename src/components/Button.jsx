cbf789fcfb81e1b7f4ad3438be3bb402bcf278df
Julie Lacresse <Julie.Lacresse@nvision.lu>
2024-08-19 09:45:26 +0200

    add handle click onboarding

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function Button({txt}) {
-  return (
-    <button className='bg-primary text-white px-12 py-6 text-button rounded-full hover:bg-secondary hover:text-primary'>{txt}</button>
-  )
+export default function Button({ txt, onClick }) {
+	return (
+		<button
+			className="bg-primary text-white px-12 py-6 text-button rounded-full hover:bg-secondary hover:text-primary"
+			onClick={onClick}
+		>
+			{txt}
+		</button>
+	);
 }
--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import Button from "../components/Button";
 import Paw from "../assets/img/paw.png";
 import Back from "../assets/img/onboarding/background.png";
@@ -7,6 +8,36 @@ import Dog from "../assets/img/onboarding/dogs.png";
 import Step from "../assets/img/onboarding/icones/footer_btn.png";
 
 export default function Onboarding() {
+	const [currentStep, setCurrentStep] = useState(0);
+	const steps = [
+    {
+      title: (
+        <>
+          Find a <span className="text-secondary">Dog Park</span> near you!
+        </>
+      ),
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    },
+		{
+			title: "Connect with Dog Lovers",
+			description:
+				"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		},
+		{
+			title: "Track Your Dog's Activity",
+			description:
+				"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		},
+	];
+
+	const handleNextBtn = () => {
+		if (currentStep < steps.length - 1) {
+			setCurrentStep(currentStep + 1);
+		} else {
+			console.log("Onboarding terminé");
+		}
+	};
+
 	return (
 		<div className="flex flex-col justify-center items-center mx-8 text-center h-screen">
 			<div className="w-full mb-4">
@@ -21,14 +52,14 @@ export default function Onboarding() {
 				</div>
 			</div>
 
-			<h1 className="text-h1 mb-3">
-				Find a <span className="text-secondary">Dog Park</span> near you!
-			</h1>
-			<p className="mb-6 text-paragraph">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor
+			<h1 className="text-h1 mb-3 font-poppins">{steps[currentStep].title}</h1>
+			<p className="mb-6 text-paragraph font-poppins">
+				{steps[currentStep].description}
 			</p>
-			<Button txt="Let's go" />
+			<Button
+				txt={currentStep < steps.length - 2 ? "Let's go" : "Next"}
+				onClick={handleNextBtn}
+			/>
 			<div className="mt-5">
 				<img src={Step} alt="step one" />
 			</div>
