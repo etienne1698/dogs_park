bfc3d73301286dc9659000cd3adb22282395b61d
Julie Lacresse <Julie.Lacresse@nvision.lu>
2024-08-19 16:34:35 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,76 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "primary": "#41245C",
-        "secondary": "#EBA059",
-        "grey":"#F1F1F1",
-        "greyText": "#7C7C7C",
-        "orangeLink":"#EBA059"
-      },
-      margin: {
-        "pageX": "1rem",
-        "pageY": "5rem"
-      },
-    },
-    fontSize: {
-      h1: [
-        "32px",
-        {
-          lineHeight: "44px",
-          fontWeight: "700",
-        },
-      ],
-      button: [
-        "16px",
-        {
-          lineHeight: "24px",
-          fontWeight: "500",
-        },
-      ],
-    },
-  },
-  plugins: [],
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-			colors: {
-				primary: "#41245C",
-				secondary: "#EBA059",
-				grey: "#F1F1F1",
-			},
-			margin: {
-				page: "1rem",
-			},
+	  extend: {
+		colors: {
+		  primary: "#41245C",
+		  secondary: "#EBA059",
+		  grey: "#F1F1F1",
+		  greyText: "#7C7C7C",
+		  orangeLink: "#EBA059"
+		},
+		margin: {
+		  pageX: "1rem",
+		  pageY: "5rem",		  
+		},
+		fontFamily: {
+		  poppins: ['Poppins', 'sans-serif'],
 		},
 		fontSize: {
-			h1: [
-				"32px",
-				{
-					lineHeight: "44px",
-					fontWeight: "700",
-				},
-			],
-			button: [
-				"16px",
-				{
-					lineHeight: "24px",
-					fontWeight: "500",
-				},
-			],
-			paragraph: [
-				"14px",
-				{
-					lineHeight: "21px",
-					fontWeight: "400",
-				},
-			],
+		  h1: [
+			"32px",
+			{
+			  lineHeight: "44px",
+			  fontWeight: "700",
+			},
+		  ],
+		  button: [
+			"16px",
+			{
+			  lineHeight: "24px",
+			  fontWeight: "500",
+			},
+		  ],
+		  paragraph: [
+			"14px",
+			{
+			  lineHeight: "21px",
+			  fontWeight: "400",
+			},
+		  ],
 		},
+	  },
 	},
 	plugins: [],
-};
+  };
+  
