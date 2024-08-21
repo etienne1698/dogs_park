7bbe0f68c0b3a0e5a2d7eec63a592cc9a6cb7834
Julie Lacresse <Julie.Lacresse@nvision.lu>
2024-08-21 10:09:20 +0200

    change to navlink

--- src/components/Menu/MenuItem.jsx
+++ src/components/Menu/MenuItem.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function MenuItem({ icon, text, to }) {
 	return (
 		<div className="flex items-center">
-			<div className="">
+			<NavLink
+				to={to}
+				className={({ isActive }) =>
+					`flex flex-col items-center space-y-1 ${
+						isActive ? "text-primary" : "text-gray-500"
+					}`
+				}
+			>
 				<img src={icon} alt={text} className="w-6 h-6 mx-auto cursor-pointer" />
-				<Link to={to} className="text-gray-500 font-poppins font-medium">
-					{text}
-				</Link>
-			</div>
+				<span className="font-poppins font-medium">{text}</span>
+			</NavLink>
 		</div>
 	);
 }
